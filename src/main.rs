c2decc9da1bffbf50f29a116d6276f094fc6ebee
abdo <abdelrahmanorigin@gmail.com>
2024-11-07 13:49:32 +0200

    Refactor + Error handling
    extract main into fucntion
    error handling
    replace tabs "\t" with FOUR_SPACES
    add #[allow(dead_code) for the struct Solution

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,43 @@ use scraper::{Html, Selector};
 use std::{fs::File, io::Write, process::Command};
 use thiserror::Error;
 
+const FOUR_SPACES: &str = "    ";
+
+fn main() -> Result<(), anyhow::Error> {
+    let leetcode_api_response = leetcode_reqwest().context("Failed api request")?;
+
+    let question_link = format!("https://leetcode.com{}", leetcode_api_response.link);
+    let question_content = leetcode_api_response.question.content;
+
+    // Rust is the 15 indexed in the code_snippets vec
+    // check if it is rust
+    let code_snippet = &leetcode_api_response.question.code_snippets[15];
+    if code_snippet.lang != "Rust" {
+        return Err(anyhow::Error::msg("not Rust!, 7asal 5er"));
+    }
+
+    // file path
+    let title_slug = leetcode_api_response.question.title_slug;
+    let question_id = leetcode_api_response.question.question_id;
+    let difficulty = leetcode_api_response.question.difficulty.to_lowercase();
+
+    let file_path = format!("{}_{}_{}", title_slug, question_id, difficulty);
+    execute_command("cargo new --lib", &file_path).context(format!(
+        "Failed to create new cargo library `{}`",
+        file_path
+    ))?;
+
+    let lib_file_path = format!("{}/src/lib.rs", file_path);
+    execute_command("echo '' >", &lib_file_path)
+        .context(format!("Failed to clear contents of `{}`", lib_file_path))?;
+
+    let file_content = generate_file_contents(&question_content, &question_link, code_snippet);
+
+    write_to_lib_file(&file_content, &lib_file_path)?;
+
+    Ok(())
+}
+
 use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 struct LeetcodeApi {
@@ -59,160 +96,156 @@ fn leetcode_reqwest() -> Result<DailyQuestion, ReqwestApiError> {
     Ok(decoding_json.data.active_daily_coding_challenge_question)
 }
 
-fn main() -> Result<(), anyhow::Error> {
-    let leetcode_api_response = leetcode_reqwest().context("Failed api request")?;
-    let question_link = format!("https://leetcode.com{}", leetcode_api_response.link);
+#[derive(Error, Debug)]
+enum CommandError {
+    #[error("Failed to execute the `{0}` process")]
+    ExecuteProcessError(#[from] std::io::Error),
+    #[error("Command `{0}` failed to execute successfully")]
+    CommandExecutionError(String),
+}
 
-    let question_response = leetcode_api_response.question;
-    let question_id = question_response.question_id;
-    let title_slug = question_response.title_slug;
-    let difficulty = question_response.difficulty.to_lowercase();
-    let quesion_content = question_response.content;
-    let code_snippet = &question_response.code_snippets[15];
-    // check if it is rust
-    let lang = &code_snippet.lang;
-    if lang != "Rust" {
-        println!("not Rust!, 7asal 5er");
-        return Ok(());
+fn execute_command(command: &str, file_path: &str) -> Result<(), CommandError> {
+    let status = Command::new("sh")
+        .arg("-c")
+        .arg(format!("{} {}", command, file_path))
+        .status()
+        .map_err(CommandError::ExecuteProcessError)?;
+
+    if status.success() {
+        println!("Command `{} {}` Successfully executed", command, file_path);
+        Ok(())
+    } else {
+        Err(CommandError::CommandExecutionError(format!(
+            "`{} {}`",
+            command, file_path
+        )))
     }
+}
 
-    let document = Html::parse_document(&quesion_content);
+fn generate_file_contents(
+    question_content: &str,
+    question_link: &str,
+    code_snippet: &Lang,
+) -> String {
+    let examples = extract_examples(question_content);
 
-    // sometimes it's <pre> sometimes it's <example-io> classes
-    let example_selector = Selector::parse(".example-io").unwrap();
+    let (test_cases, function_signature) = generate_test_cases(code_snippet, examples);
+
+    let file_content = format!(
+        r#"// {question_link}
+#[allow(dead_code)]
+struct Solution;
+
+#[allow(dead_code)]
+{function_signature}
+
+#[cfg(test)]
+mod tests {{
+    use super::*;{test_cases}
+}}
+"#
+    );
+
+    file_content
+}
+
+fn extract_examples(question_content: &str) -> Vec<String> {
+    let document = Html::parse_document(&question_content);
+
+    let pre_selector = Selector::parse("pre").unwrap();
 
     let mut examples: Vec<String> = Vec::new();
 
-    let examples_selector: Vec<_> = document.select(&example_selector).collect();
-    if !examples_selector.is_empty() {
+    if document.select(&pre_selector).next().is_some() {
+        for input_output in document.select(&pre_selector) {
+            let input_output: String = input_output.text().collect();
+            let mut input_output_parts = input_output.split('\n');
+            let input = input_output_parts
+                .next()
+                .unwrap_or("")
+                .replace("Input:", "");
+            let output = input_output_parts
+                .next()
+                .unwrap_or("")
+                .replace("Output:", "");
+
+            let formatted = format!(
+                "let{};\n{FOUR_SPACES}{FOUR_SPACES}let output ={};",
+                input, output
+            );
+            let formatted = add_vec_and_to_string(formatted);
+            examples.push(formatted);
+        }
+    } else {
+        let example_io_selector = Selector::parse(".example-io").unwrap();
+        let examples_selector: Vec<_> = document.select(&example_io_selector).collect();
+
         let mut i = 0;
         while i < examples_selector.len() {
             let input = examples_selector[i].text().collect::<String>();
             let output = examples_selector[i + 1].text().collect::<String>();
-            let formatted = format!("let {};\n\t\tlet output = {};", input, output);
-            // add vecs
-            let formatted = formatted.replace("[", "vec![");
-            // add .to_string()
-            let mut new_formatted = String::new();
-            let mut skip = true;
-            for letter in formatted.chars() {
-                if letter == '"' {
-                    if skip {
-                        skip = false;
-                    } else {
-                        new_formatted.push_str("\".to_string()");
-                        skip = true;
-                        continue;
-                    }
-                }
-                new_formatted.push(letter);
-            }
-            examples.push(new_formatted);
-            dbg!(input);
-            dbg!(output);
+
+            let formatted = format!(
+                "let {};\n{FOUR_SPACES}{FOUR_SPACES}let output = {};",
+                input, output
+            );
+            let formatted = add_vec_and_to_string(formatted);
+            examples.push(formatted);
             i += 2;
         }
-    } else {
-        let example_selector = Selector::parse("pre").unwrap();
-        // let examples_selector: Vec<_> = document.select(&example_selector).collect();
-        for wow in document.select(&example_selector) {
-            let text: String = wow.text().collect();
-            let mut parts = text.split('\n');
-            let input = parts.next().unwrap();
-            let output = parts.next().unwrap();
+    }
 
-            // remove Input:
-            let input = input.replace("Input:", "");
-            // remove Output:
-            let output = output.replace("Output:", "");
+    examples
+}
 
-            let formatted = format!("let{};\n\t\tlet output ={};", input, output);
-            // add vecs
-            let formatted = formatted.replace("[", "vec![");
-            // add .to_string()
-            let mut new_formatted = String::new();
-            let mut skip = true;
-            for letter in formatted.chars() {
-                if letter == '"' {
-                    if skip {
-                        skip = false;
-                    } else {
-                        new_formatted.push_str("\".to_string()");
-                        skip = true;
-                        continue;
-                    }
-                }
-                new_formatted.push(letter);
+fn add_vec_and_to_string(input: String) -> String {
+    let mut new_formatted = String::new();
+    let mut skip = true;
+    for letter in input.replace("[", "vec![").chars() {
+        if letter == '"' {
+            if skip {
+                skip = false;
+            } else {
+                new_formatted.push_str("\".to_string()");
+                skip = true;
+                continue;
             }
-            examples.push(new_formatted);
-            dbg!(input);
-            dbg!(output);
         }
+        new_formatted.push(letter);
     }
+    new_formatted
+}
 
-    let file_path = format!("{}_{}_{}", title_slug, question_id, difficulty);
+fn generate_test_cases(code_snippet: &Lang, examples: Vec<String>) -> (String, String) {
+    let function_signature = code_snippet.code.replace(
+        &format!("\n{}{}\n", FOUR_SPACES, FOUR_SPACES),
+        &format!("\n{}{}todo!();\n", FOUR_SPACES, FOUR_SPACES),
+    );
 
-    let create_cargo_lib = Command::new("sh")
-        .arg("-c")
-        .arg(format!("cargo new --lib {}", file_path))
-        .status()
-        .unwrap();
-
-    if create_cargo_lib.success() {
-        println!("Successfully created {}", file_path);
-    } else {
-        eprintln!("Failed to create {}", file_path);
-        return Ok(());
-    }
-
-    let lib_file_path = format!("{}/src/lib.rs", file_path);
-
-    let delete_lib_content = Command::new("sh")
-        .arg("-c")
-        .arg(format!("echo '' > {}", lib_file_path))
-        .status()
-        .unwrap();
-
-    if delete_lib_content.success() {
-        println!("Successfully cleared {}", lib_file_path);
-    } else {
-        eprintln!("Failed to clear {}", lib_file_path);
-        return Ok(());
-    }
-
-    // File::create() docs
-    // `This function will create a file if it does not exist,
-    // and will truncate it if it does.`
-    // "truncate" means clear its contents and start fresh if it does exist
-    let mut lib_file = File::create(&lib_file_path).expect("Failed to create lib.rs");
-
-    let function_signature = &code_snippet.code;
-
-    let function_signature =
-        function_signature.replace("{\n        \n    }\n}", "{\n\t\ttodo!();\n\t}\n}");
-
-    let first_bracket_in_func_signature = function_signature.find('(').unwrap();
+    let first_bracket_in_func_signature = function_signature
+        .find('(')
+        .expect("for some reason there is no `(`");
     let pure_function_name = &function_signature[27..first_bracket_in_func_signature];
-    let last_bracket_in_func_signature = function_signature.find(')').unwrap();
+    let last_bracket_in_func_signature = function_signature
+        .find(')')
+        .expect("for some reason there is no `)`");
 
-    let slice =
+    let function_params =
         &function_signature[first_bracket_in_func_signature + 1..last_bracket_in_func_signature];
 
-    let parts = slice.split_whitespace().collect::<Vec<_>>();
+    let function_params_parts = function_params.split_whitespace().collect::<Vec<_>>();
 
     let mut function_name = String::new();
 
     let mut i = 0;
-    while i < parts.len() {
-        if i == parts.len() - 2 {
-            function_name.push_str(&parts[i].replace(":", ""));
+    while i < function_params_parts.len() {
+        if i == function_params_parts.len() - 2 {
+            function_name.push_str(&function_params_parts[i].replace(":", ""));
         } else {
-            function_name.push_str(&parts[i].replace(":", ", "));
+            function_name.push_str(&function_params_parts[i].replace(":", ", "));
         }
         i += 2;
     }
-
-    dbg!(&function_name);
 
     let mut test_cases = String::new();
 
@@ -231,84 +264,62 @@ fn main() -> Result<(), anyhow::Error> {
     fn it_works{idx}() {{
         {example}
         let result = Solution::{pure_function_name}({function_name});
+        assert_eq!(result, output);
     }}"#
         );
         test_cases.push_str(&test_case);
     }
 
-    let file_content = format!(
-        r#"// {question_link}
-struct Solution;
+    (test_cases, function_signature)
+}
 
-{function_signature}
+#[derive(Error, Debug)]
+enum CreateWriteLibFileError {
+    #[error("Failed to create aka clear lib.rs")]
+    CreateClearLibFile(std::io::Error),
+    #[error("Failed to write to lib.rs")]
+    WriteToLibFile(std::io::Error),
+}
 
-#[cfg(test)]
-mod tests {{
-    use super::*;{test_cases}
-}}
-"#
-    );
+fn write_to_lib_file(
+    file_content: &str,
+    lib_file_path: &str,
+) -> Result<(), CreateWriteLibFileError> {
+    // File::create() docs
+    // This function will create a file if it does not exist,
+    // and will truncate it if it does.
+    // "truncate" means clear its contents and start fresh if it does exist
+    let mut lib_file =
+        File::create(lib_file_path).map_err(CreateWriteLibFileError::CreateClearLibFile)?;
 
+    // Write the content to the file, handling any write errors.
     lib_file
         .write_all(file_content.as_bytes())
-        .expect("Failed to write to lib.rs");
+        .map_err(CreateWriteLibFileError::WriteToLibFile)?;
 
     println!("Successfully wrote to lib {}", lib_file_path);
-    // TODO: adjust test cases to use the @ c
-    // from this
-    // //Example 1:
-    // Input: s = "abcde", goal = "cdeab"
-    // Output: true
-    //
-    // //Example 2:
-    // Input: s = "abcde", goal = "abced"
-    // Output: false
-    //
-    // to this
-    // //Example 1:
-    // let s = "abcde", goal = "cdeab";
-    // let output =  true;
-    //
-    // //Example 2:
-    // let s = "abcde", goal = "abced";
-    // let output =  false;
-    //
-    // then adjust it more with doing the strings and vecs
-    // //Example 1:
-    // let Input: s = "abcde".to_string(), goal = "cdeab".to_string();
-    // let output =  true;
-    //
-    // //Example 2:
-    // let s = "abcde", goal = "abced";
-    // let output =  false;
-    //
-    // then lastly try  to remove the commas, but idk how would be done for now
-    //
-    // rust fmt at the end of the program, if all not formatted
-    //
-    // remove "Example 1, 2, 3" doesn't add anything
-    //
-    // TODO: for now
-    // 1 - [ ] Input output
-    // - [x] remove input and output and add ; at the end
-    // - [x] adjust the vecs by replacing '[' with 'vec!['
-    // - [x] adjust the strings by replacing to every second '"' with '".to_string()'
-    // - [ ] replace commas that are not in the input or in the vecs with '; let '
-    //
-    // TODO:
-    // 2 - [x] Solution::func_name(params)
-    // - [x] Solution::()
-    // - [x] parse the function signature to split the between the brackets "()" by ','
-    // - [x] get the index of '(' and the end of ')' and split by ',' for what's between them
-    //
-    // TODO:
-    // 3 - simple cleaning
-    // - [ ] replace all "\t" with spaces
-    // - [ ] add 2 allow deadcode above struct Solution and impl Solution
-    //
-    // TODO:
-    // 4 - Error handling
-    // - [x] reqwest
-    // - [ ] the rest
     Ok(())
 }
+
+// TODO:
+// 1 - [ ] Input output
+// - [x] remove input and output and add ; at the end
+// - [x] adjust the vecs by replacing '[' with 'vec!['
+// - [x] adjust the strings by replacing to every second '"' with '".to_string()'
+// - [ ] replace commas that are not in the input or in the vecs with '; let '
+//
+// TODO:
+// 2 - [x] Solution::func_name(params)
+// - [x] Solution::()
+// - [x] parse the function signature to split the between the brackets "()" by ','
+// - [x] get the index of '(' and the end of ')' and split by ',' for what's between them
+//
+// TODO:
+// 3 - [x] simple cleaning
+// - [x] replace all "\t" with spaces
+// - [x] add 2 allow deadcode above struct Solution and impl Solution
+//
+// TODO:
+// 4 - [x] Error handling
+// - [x] reqwest
+// - [x] the rest
